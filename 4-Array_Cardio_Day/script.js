b9ebf1f7a6eae5b4715e64fc82552b2964bdc85e
leyli00 <leylidostluk@gmail.com>
2024-10-25 17:34:23 +0300

    test

---
+++ 4-Array_Cardio_Day/script.js
@@ -0,0 +1,19 @@
+
+
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, ,Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in 1500's
+
+// Array.prototype.map()
+// 2. Give us an array of the inventory first and last names
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+// Array.prototype.
